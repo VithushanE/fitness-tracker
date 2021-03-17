2096cb791b240782c632ac80bb5f7fd985ab1392
Vithushan <vithushan@Vithushans-MacBook-Pro.local>
2021-03-17 01:10:45 -0400

    created folder structure

---
+++ Develop/server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const logger = require("morgan");
+const mongoose = require("mongoose");
+require('dotenv').config();   
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
